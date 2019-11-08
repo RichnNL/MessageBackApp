bdeebb7f6cb67c20bc3c21afa9be63352cc67435
Dyer,Richard R.J <r.dyer@student.fontys.nl>
2019-11-08 10:19:16 +0100

    Fixed for on heroku

--- Database/SqlDatabase.php
+++ Database/SqlDatabase.php
@@ -3,6 +3,7 @@
 namespace Database;
 
 use \PDO;
+use\mysqli;
 use \PDOException;
 
 class SqlDatabase {
@@ -15,16 +16,13 @@ class SqlDatabase {
   private $dbname;
   private $username;
   private $password;
+  private $pdoArray = null;
   
   private static $database = NULL;
   // Database is a Singelton for accessibility and balance
   private function __construct()
   {
-      $this->databaseType = getenv("DB_CONNECTION");
-      $this->host = getenv("DB_HOST");
-      $this->dbname = getenv("DB_DATABASE");
-      $this->username =  getenv("DB_USERNAME");
-      $this->password =  getenv("DB_PASSWORD");
+     
       $this->connect();
   }
   
@@ -45,8 +43,13 @@ class SqlDatabase {
   public function connect() {
       try {
         $location = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname ;
-        $this->connection = new PDO($location, $this->username, $this->password);
-        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        if($this->pdoArray != null) {
+          $this->connection = new mysqli($location, $this->username, $this->password, $this->pdoArray);
+        } else {
+          $this->connection = new PDO($location, $this->username, $this->password);
+          $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+       
         $this->initTables();
       } catch(PDOException $error) {
           echo 'Connection Error: ' . $error->getMessage();
@@ -71,6 +74,23 @@ class SqlDatabase {
       ) ";
 
       $this->connection->query($createMessageTable);
+    }
+
+    private function initDatabaseConfig() {
+      if(getenv("PRODUCTION")) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $this->host = $url["host"];
+        $this->usernam = $url["user"];
+        $this->password = $url["pass"];
+        $this->pdoArray = substr($url["path"], 1);
+      } else {
+        $this->databaseType = getenv("DB_CONNECTION");
+        $this->host = getenv("DB_HOST");
+        $this->dbname = getenv("DB_DATABASE");
+        $this->username =  getenv("DB_USERNAME");
+        $this->password =  getenv("DB_PASSWORD");
+      }
+      
     }
 
   }
